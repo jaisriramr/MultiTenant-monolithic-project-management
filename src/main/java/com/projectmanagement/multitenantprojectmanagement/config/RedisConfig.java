d9a28f1c5da7457b566ba58d8c7583e3266b768a
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-11 23:30:58 +0530

    Configurations added

---
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.projectmanagement.multitenantprojectmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.activateDefaultTyping(
+            LaissezFaireSubTypeValidator.instance,
+            ObjectMapper.DefaultTyping.NON_FINAL
+        );
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        serializer.setObjectMapper(mapper);
+
+        template.setDefaultSerializer(serializer);
+        return template;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                    .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+}
---
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/interceptor/RateLimitInterceptor.java
@@ -0,0 +1,40 @@
+package com.projectmanagement.multitenantprojectmanagement.interceptor;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.projectmanagement.multitenantprojectmanagement.config.CacheService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    private final CacheService cacheService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String key = "rate_limit:" + request.getRemoteAddr();
+        int maxRequests = 10;
+        int timeWindow = 60;
+
+        if(!cacheService.isAllowed(key, maxRequests, timeWindow)) {
+            response.setStatus(429);;
+            response.setContentType("application/json");
+            try {
+                response.getWriter().write("Rate limit exceeded. Try again later");
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+        return true;
+
+    }
+
+}
