8b1faf30fcd3416dbf56713069a11046fce8d692
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-11 16:28:29 +0530

    Initial commit - Spring Boot Project Setup

---
+++ src/main/java/com/projectmanagement/multi_tenant_project_management/MultiTenantProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.projectmanagement.multi_tenant_project_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectmanagement/multi_tenant_project_management/MultiTenantProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmanagement.multi_tenant_project_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
