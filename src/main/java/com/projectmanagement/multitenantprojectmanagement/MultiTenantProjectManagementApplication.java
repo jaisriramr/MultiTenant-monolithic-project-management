b15a89be8ecd3d1eb2dff3354da58d47d902b5df
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-11 16:55:54 +0530

    RBAC md added

---
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/MultiTenantProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.projectmanagement.multitenantprojectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantProjectManagementApplication.class, args);
+	}
+
+}
