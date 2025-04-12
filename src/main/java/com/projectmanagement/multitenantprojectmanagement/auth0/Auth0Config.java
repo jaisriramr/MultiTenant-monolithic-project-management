5de83b14517e70c6d26e8125837f2366119f38e7
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-13 01:46:35 +0530

    auth0 client fix

--- src/main/java/com/projectmanagement/multitenantprojectmanagement/auth0/Auth0Config.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/auth0/Auth0Config.java
@@ -1,39 +1,31 @@
 package com.projectmanagement.multitenantprojectmanagement.auth0;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.Getter;
+import lombok.Data;
 
 @Configuration
 @ConfigurationProperties(prefix = "auth0")
-@Getter
+@Data
 public class Auth0Config {
 
-    @Value("${auth0.domain}")
-    private String auth0Domain;
+    private String domain;
 
-    @Value("${auth0.client_id}")
     private String clientId;
 
-    @Value("${auth0.client_secret}")
     private String clientSecret;
 
-    @Value("${auth0.audience}")
     private String audience;
 
-    @Value("${auth0.identifier}")
     private String identifier;
 
-    @Value("${auth0.api_id}")
-    private String api_id;
+    private String apiId;
 
-    @Value("${auth0.connection_id}")
-    private String connection_id;
+    private String connectionId;
 
     @Bean(name = "auth0RestTemplate")
     public RestTemplate auth0RestTemplate() {
--- src/main/java/com/projectmanagement/multitenantprojectmanagement/auth0/Auth0Service.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/auth0/Auth0Service.java
@@ -2,10 +2,8 @@ package com.projectmanagement.multitenantprojectmanagement.auth0;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -34,7 +32,7 @@ public class Auth0Service {
                 "invitee", invitee,
                 "roles", List.of(roleId));
 
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/organizations/" + orgId + "/invitations";
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/organizations/" + orgId + "/invitations";
 
         Map<String, Object> response = auth0Client.makeApiRequest(HttpMethod.POST, url, requestBody, true).getBody();
 
@@ -47,14 +45,14 @@ public class Auth0Service {
     }
 
     public void revokeInvitationSentToAnUser(String orgId, String invitationId) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/organizations/" + orgId + "/invitations/"
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/organizations/" + orgId + "/invitations/"
                 + invitationId;
 
         auth0Client.makeApiRequest(HttpMethod.DELETE, url, null, true);
     }
 
     public ResponseEntity<Map<String, Object>> createAnOrganization(String name, String displayName) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/organizations";
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/organizations";
 
         String params = "{"
                 + "\"name\":\"" + name + "\","
@@ -68,7 +66,7 @@ public class Auth0Service {
                 + "},"
                 + "\"metadata\":{},"
                 + "\"enabled_connections\":[{"
-                + "\"connection_id\":\"" + auth0Config.getConnection_id() + "\","
+                + "\"connection_id\":\"" + auth0Config.getConnectionId() + "\","
                 + "\"assign_membership_on_login\":true,"
                 + "\"show_as_button\":true,"
                 + "\"is_signup_enabled\":true"
@@ -87,7 +85,7 @@ public class Auth0Service {
     }
 
     public void updateAnOrganization(String orgId, String name, String displayName) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/organizations/" + orgId;
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/organizations/" + orgId;
         String params = "{\"name\":\""+ name +"\",\"display_name\":\""+ name +"\",\"branding\":{\"logo_url\":\"https://images.unsplash.com/photo-1657807783435-a352520a37b1\",\"colors\":{\"primary\":\"#ffffff\",\"page_background\":\"#f4f4f4\"}},\"metadata\":{}}";
 
         try {
@@ -102,12 +100,12 @@ public class Auth0Service {
     }
 
     public void deleteAnOrganization(String orgId) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/organizations/" + orgId;
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/organizations/" + orgId;
         auth0Client.makeApiRequest(HttpMethod.DELETE, url, null, true);
     }
 
     public ResponseEntity<Map<String, Object>> createARole(String name, String description) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/roles";
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/roles";
 
         Map<String, Object> requestBody = Map.of(
             "name", name,
@@ -119,7 +117,7 @@ public class Auth0Service {
 
     public ResponseEntity<Map<String, Object>> updateARole(String roleId, String name, String description) {
 
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/roles/" + roleId;
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/roles/" + roleId;
 
         Map<String, Object> requestBody = Map.of(
             "name", name,
@@ -130,7 +128,7 @@ public class Auth0Service {
     }
 
     public void assignRolesToUser(String userId, List<String> roles) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/users/" + userId + "/roles";
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/users/" + userId + "/roles";
 
         Map<String, Object> requestBody = Map.of("roles", roles);
 
@@ -138,7 +136,7 @@ public class Auth0Service {
     }
 
     public void removeRolesFromUser(String userId, List<String> roles) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/users/" + userId + "/roles";
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/users/" + userId + "/roles";
 
         Map<String, Object> requestBody = Map.of("roles", roles);
 
@@ -146,12 +144,12 @@ public class Auth0Service {
     }
 
     public void removeRoleFromAuth0(String roleId) {
-        String url = "https://" +  auth0Config.getAuth0Domain() + "/api/v2/roles/" + roleId;
+        String url = "https://" +  auth0Config.getDomain() + "/api/v2/roles/" + roleId;
         auth0Client.makeApiRequest(HttpMethod.DELETE, url, null, true);
     }
 
     public void assignOrRemovePermissionToARole(String roleId, List<String> permissions) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/roles/" + roleId;
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/roles/" + roleId;
     
         Stream<Object> permissionList = permissions.stream().map(permission -> Map.of("resource_server_identifier", auth0Config.getIdentifier(), "permission_name", permission));
         Map<String, Object> requestbody = Map.of("permissions", permissionList);
@@ -160,7 +158,7 @@ public class Auth0Service {
     }
 
     public ResponseEntity<Map<String, Object>> createOrUpdateOrDeletePermission(List<Map<String, String>> permissions) {
-        String url = "https://" + auth0Config.getAuth0Domain() + "/api/v2/resource-servers/" + auth0Config.getAuth0Domain();
+        String url = "https://" + auth0Config.getDomain() + "/api/v2/resource-servers/" + auth0Config.getDomain();
 
         Map<String, Object> requestBody = Map.of("permissions", permissions);
 
