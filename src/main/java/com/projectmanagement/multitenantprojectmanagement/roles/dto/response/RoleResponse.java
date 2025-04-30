99011d91ae53fe2879a139db962c92543ca7b234
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-30 17:20:46 +0530

    org invite test

--- src/main/java/com/projectmanagement/multitenantprojectmanagement/roles/dto/response/RoleResponse.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/roles/dto/response/RoleResponse.java
@@ -3,11 +3,15 @@ package com.projectmanagement.multitenantprojectmanagement.roles.dto.response;
 import java.util.Set;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoleResponse {
     private UUID id;
     private String name;
