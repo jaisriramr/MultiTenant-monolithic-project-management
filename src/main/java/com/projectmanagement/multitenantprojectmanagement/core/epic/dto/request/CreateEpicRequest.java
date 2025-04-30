95052f186380fb6b9fb3b51c6f3146ac8ee8d644
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-04-30 17:04:09 +0530

    issue test added

--- src/main/java/com/projectmanagement/multitenantprojectmanagement/core/epic/dto/request/CreateEpicRequest.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/core/epic/dto/request/CreateEpicRequest.java
@@ -2,11 +2,15 @@ package com.projectmanagement.multitenantprojectmanagement.core.epic.dto.request
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateEpicRequest {
     private String name;
     private String description;
--- src/main/java/com/projectmanagement/multitenantprojectmanagement/core/issue/dto/request/UpdateIssueRequest.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/core/issue/dto/request/UpdateIssueRequest.java
@@ -3,11 +3,15 @@ package com.projectmanagement.multitenantprojectmanagement.core.issue.dto.reques
 import java.util.Set;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateIssueRequest {
     private UUID id;
     private UUID projectId;
