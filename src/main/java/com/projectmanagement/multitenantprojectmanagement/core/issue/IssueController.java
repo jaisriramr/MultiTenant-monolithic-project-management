0edeb56eaee27cb6f41f5dd101bfaad6e1a4dda1
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-05-02 10:53:18 +0530

    issue pre authorize

--- src/main/java/com/projectmanagement/multitenantprojectmanagement/core/issue/IssueController.java
+++ src/main/java/com/projectmanagement/multitenantprojectmanagement/core/issue/IssueController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,7 @@ public class IssueController {
 
     private final IssueService issueService;
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"view:task\"}, #jwt.claims[\"org_id\"])")
     @GetMapping("/v1/issue/{id}")
     public ResponseEntity<IssueResponse> getIssueById(@PathVariable UUID id,@AuthenticationPrincipal Jwt jwt) {
         Issue issue = issueService.getIssueById(id);
@@ -41,6 +43,7 @@ public class IssueController {
         return ResponseEntity.ok(IssueMapper.toIssueResponse(issue));
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"view:task\"}, #jwt.claims[\"org_id\"])")
     @GetMapping("/v1/issue")
     public ResponseEntity<IssueResponse> getIssueByKey(@RequestParam String key,@AuthenticationPrincipal Jwt jwt) {
         IssueResponse issue = issueService.getIssueByKey(key);
@@ -48,13 +51,15 @@ public class IssueController {
         return ResponseEntity.ok(issue);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"list:task\"}, #jwt.claims[\"org_id\"])")
     @GetMapping("/v1/issue/{id}/backlog")
     public ResponseEntity<PaginatedResponseDto<ListIssuesResponse>> getProjectBacklogs(@PathVariable UUID id, Pageable pageable,@AuthenticationPrincipal Jwt jwt) {
         PaginatedResponseDto<ListIssuesResponse> issues = issueService.getBacklogIssues(id, pageable);
 
         return ResponseEntity.ok(issues);
     }
-
+    
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"list:task\"}, #jwt.claims[\"org_id\"])")
     @GetMapping("/v1/issue/{id}/sprint")
     public ResponseEntity<PaginatedResponseDto<ListIssuesResponse>> getSprintIssues(@PathVariable UUID id, Pageable pageable,@AuthenticationPrincipal Jwt jwt) {
         PaginatedResponseDto<ListIssuesResponse> issues = issueService.getIssuesesBySprintId(id, pageable);
@@ -62,6 +67,7 @@ public class IssueController {
         return ResponseEntity.ok(issues);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"list:task\"}, #jwt.claims[\"org_id\"])")
     @GetMapping("/v1/issue/{id}/child-works")
     public ResponseEntity<PaginatedResponseDto<ListIssuesResponse>> getIssueChildWorks(@PathVariable UUID id, Pageable pageable,@AuthenticationPrincipal Jwt jwt) {
         PaginatedResponseDto<ListIssuesResponse> issues = issueService.getIssueChildWorks(id, pageable);
@@ -69,6 +75,7 @@ public class IssueController {
         return ResponseEntity.ok(issues);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"link:task\"}, #jwt.claims[\"org_id\"])")
     @PostMapping("/v1/issue/link/{id}/epic/{epicId}")
     public ResponseEntity<ListIssuesResponse> linkIssueToEpic(@PathVariable UUID id, @PathVariable UUID epicId,@AuthenticationPrincipal Jwt jwt) {
         ListIssuesResponse issue = issueService.linkIssueToEpic(epicId, id);
@@ -76,6 +83,7 @@ public class IssueController {
         return ResponseEntity.ok(issue);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"unlink:task\"}, #jwt.claims[\"org_id\"])")
     @PostMapping("/v1/issue/unlink/{id}/epic")
     public ResponseEntity<ListIssuesResponse> unlinkIssueToEpic(@PathVariable UUID id,@AuthenticationPrincipal Jwt jwt) {
         ListIssuesResponse issue = issueService.unlinkIssueToEpic(id);
@@ -83,6 +91,7 @@ public class IssueController {
         return ResponseEntity.ok(issue);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"create:task\"}, #jwt.claims[\"org_id\"])")
     @PostMapping("/v1/issue")
     public ResponseEntity<ListIssuesResponse> createIssue(@Valid @RequestBody CreateIssueRequest createIssueRequest,@AuthenticationPrincipal Jwt jwt) {
         ListIssuesResponse response = issueService.createIssue(createIssueRequest);
@@ -90,6 +99,7 @@ public class IssueController {
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"create:task\"}, #jwt.claims[\"org_id\"])")
     @PostMapping("/v1/issue/sub-task")
     public ResponseEntity<ListIssuesResponse> createSubIssue(@Valid @RequestBody CreateSubIssueRequest createSubIssueRequest,@AuthenticationPrincipal Jwt jwt) {
         ListIssuesResponse response = issueService.createSubIssue(createSubIssueRequest);
@@ -97,13 +107,15 @@ public class IssueController {
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"create:task\"}, #jwt.claims[\"org_id\"])")
     @PostMapping("/v1/issue/epic")
     public ResponseEntity<ListIssuesResponse> createEpicIssue(@Valid @RequestBody CreateEpicIssueRequest createEpicIssueRequest,@AuthenticationPrincipal Jwt jwt) {
         ListIssuesResponse response = issueService.createEpicIssue(createEpicIssueRequest);
 
         return ResponseEntity.ok(response);
     }
-
+    
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"update:task\"}, #jwt.claims[\"org_id\"])")
     @PutMapping("/v1/issue")
     public ResponseEntity<IssueResponse> updateIssue(@Valid @RequestBody UpdateIssueRequest updateIssueRequest,@AuthenticationPrincipal Jwt jwt) {
         IssueResponse response = issueService.updateIssue(updateIssueRequest);
@@ -111,6 +123,7 @@ public class IssueController {
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"assign:task\"}, #jwt.claims[\"org_id\"])")
     @PutMapping("/v1/issue/{id}/assign/{userId}/user")
     public ResponseEntity<String> assigneeUserToAnIssue(@PathVariable UUID id, @PathVariable UUID userId,@AuthenticationPrincipal Jwt jwt) {
         issueService.assigneeUserToAnIssue(id, userId);
@@ -118,6 +131,8 @@ public class IssueController {
         return ResponseEntity.ok("User successfully assigned to the given User ID: "+ id);
     }
 
+    
+    @PreAuthorize("@organizationMembersService.hasPermission(#jwt.subject, {\"assign:task\"}, #jwt.claims[\"org_id\"])")
     @PutMapping("/v1/issue/{id}/unassign")
     public ResponseEntity<String> unassigneeUserToAnIssue(@PathVariable UUID id,@AuthenticationPrincipal Jwt jwt) {
         issueService.unAssigneeUserToAnIssue(id);
